16e08086797f7fb7477a0cf1149079761620ec69
Victor Matheus <36866913+Vonttech@users.noreply.github.com>
2020-05-13 18:15:38 -0300

    Simple_HomePage

--- popUp.js
+++ popUp.js
@@ -1,187 +1,75 @@
-/*row 0*/
+//Botões
+let btn1 = document.getElementById('btn1')
+let btn2 = document.getElementById('btn2')
+let btn3 = document.getElementById('btn3')
+let btn4 = document.getElementById('btn4')
+let btn5 = document.getElementById('btn5')
 
-//Define qual botão será selecionado
-function getButtonId(id) {
-    var btnId = id;
+let btnArray = [btn1, btn2, btn3, btn4, btn5]
 
-    switch (id) {
-        case '1':
-            choose_0()
-            break;
-
-        default:
-            break;
-    }
+/*Adicionando aos botões 
+    classes
+    eventos
+    estado
+*/
+function addFeatures(item) {
+    item.className = 'btn font-weight-bold'
+    item.setAttribute('state', 'notClicked')
+    item.addEventListener('mouseover', function () { mouseIn(item.id) })
+    item.setAttribute('onmouseout', 'mouseOut(id)')
+    item.addEventListener('click', function () { clicado(item.id) })
 }
+btnArray.forEach(addFeatures)
 
 
-
-
-let right_col_0 = document.getElementById('area-text-0')
-let clickCount = 0
-let btn = document.getElementById('btn1')
-btn.addEventListener('mouseover', show_0)
-btn.addEventListener('mouseout', notShow_0)
-btn.addEventListener('click', choose_0)
-
-function show_0() {
-    right_col_0.innerHTML = '<input type="text" id="input-0" class = "conjunto"><button id="inputBtn-0" class = "btn ml-1 p-0 pl-1 pr-1 conjunto">Go</button>'
-    btn.style.background = "rgb(82, 126, 247)"
-    btn.style.border = 'solid'
-    btn.style.borderRadius = '50px'
+//Efeitos ao botão
+function mouseIn(id) {
+    var btn = document.getElementById(id)
     btn.style.color = 'white'
     btn.style.fontSize = '25px'
-    btn.style.padding = '10px'
+    btn.style.background = 'rgb(82, 126, 247)'
     btn.style.transition = '0.7s'
+    btn.style.borderRadius = '30px'
+    btn.style.padding = '10px'
 }
 
-function notShow_0() {
-    document.getElementById('input-0').remove()
-    document.getElementById('inputBtn-0').remove()
-    btn.style.background = "white"
-    btn.style.borderRadius = '50px'
+function mouseOut(id) {
+    var btn = document.getElementById(id)
     btn.style.color = '#212529'
     btn.style.fontSize = '16px'
-    btn.style.padding = '6px 12px'
+    btn.style.background = 'none'
     btn.style.transition = '0.7s'
-    btn.style.border = 'none'
+    btn.style.borderRadius = '0px'
+    btn.style.padding = '6px 12px'
 }
 
-//Aleta o estilo caso o botão seja clicado
-function choose_0() {
-    clickCount++
-    right_col_0.innerHTML = '<input type="text" id="inputChoosed" class = "conjunto"><button id="inputBtnChoosed" class = "btn ml-1 p-0 pl-1 pr-1 conjunto">Go</button>'
-    btn.style.background = "rgb(82, 126, 247)"
-    btn.style.borderRadius = '50px'
-    btn.style.color = 'white'
-    btn.style.fontSize = '25px'
-    btn.style.padding = '10px'
+function clicado(id) {
+    var btn = document.getElementById(id)
+    btn.setAttribute('onmouseout', '')
+    if (btn.getAttribute('state') == 'notClicked') {
+        btn.setAttribute('state', 'clicked')
+        btn.style.color = 'white'
+        btn.style.fontSize = '25px'
+        btn.style.background = 'rgb(82, 126, 247)'
+        btn.style.borderRadius = '30px'
+        btn.style.padding = '10px'
 
-    //Chama a função para fechar a opção
-    if (clickCount == 2) {
-        close()
-        clickCount = 0;
+    } else if (btn.getAttribute('state') == 'clicked') {
+        btn.setAttribute('state', 'notClicked')
+        btn.setAttribute('onmouseout', 'mouseOut(id)')
+        btn.style.color = '#212529'
+        btn.style.fontSize = '16px'
+        btn.style.background = 'none'
+        btn.style.transition = '0.7s'
+        btn.style.borderRadius = '0px'
+        btn.style.padding = '6px 12px'
     }
+
 }
 
 
-//Retorna para o estilo padrão de maneira suave
-function close() {
-    document.getElementById('inputChoosed').remove()
-    document.getElementById('inputBtnChoosed').remove()
-    btn.style.background = "white"
-    btn.style.borderRadius = '50px'
-    btn.style.color = '#212529'
-    btn.style.fontSize = '16px'
-    btn.style.padding = '6px 12px'
-    btn.style.transition = '0.7s'
-    btn.style.border = 'none'
-}
 
 
-/*row 1*/
-let right_col_1 = document.getElementById('area-text-1')
 
-function show_1() {
-    right_col_1.innerHTML = '<input type="text" id="input-1" class = "conjunto"><button id="inputBtn-1" class = " btn ml-1 p-0 pl-1 pr-1 conjunto">Go</button>'
-}
-
-function notShow_1() {
-    document.getElementById('input-1').remove()
-    document.getElementById('inputBtn-1').remove()
-
-    var btn = document.getElementById('btn1')
-    btn.className = 'btn font-weight-bold'
-    btn.style.transition = '0.7s'
-}
-
-function choose_1(params) {
-    right_col_1.innerHTML = '<input type="text" id="inputChoosed" class = "conjunto"><button id="inputBtnChoosed" class = " btn ml-1 p-0 pl-1 pr-1 conjunto">Go</button>'
-    var btn = document.getElementById('btn1')
-    btn.style.background = "rgb(82, 126, 247)"
-    btn.style.borderRadius = '50px'
-    btn.style.color = 'white'
-    btn.style.fontSize = '25px'
-    btn.style.padding = '10px'
-}
-
-/*row 2*/
-
-let right_col_2 = document.getElementById('area-text-2')
-
-function show_2() {
-    right_col_2.innerHTML = '<input type="text" id="input-2" class = "conjunto"><button id="inputBtn-2" class = " btn ml-1 p-0 pl-1 pr-1 conjunto">Go</button>'
-}
-
-function notShow_2() {
-    document.getElementById('input-2').remove()
-    document.getElementById('inputBtn-2').remove()
-
-    var btn = document.getElementById('btn2')
-    btn.className = 'btn font-weight-bold'
-    btn.style.transition = '0.7s'
-}
-
-function choose_2(params) {
-    right_col_2.innerHTML = '<input type="text" id="inputChoosed" class = "conjunto"><button id="inputBtnChoosed" class = " btn ml-1 p-0 pl-1 pr-1 conjunto">Go</button>'
-    var btn = document.getElementById('btn2')
-    btn.style.background = "rgb(82, 126, 247)"
-    btn.style.borderRadius = '50px'
-    btn.style.color = 'white'
-    btn.style.fontSize = '25px'
-    btn.style.padding = '10px'
-}
-
-/*row 3*/
-let right_col_3 = document.getElementById('area-text-3')
-
-function show_3() {
-    right_col_3.innerHTML = '<input type="text" id="input-3" class = "conjunto"><button id="inputBtn-3" class = " btn ml-1 p-0 pl-1 pr-1 conjunto">Go</button>'
-}
-
-function notShow_3() {
-    document.getElementById('input-3').remove()
-    document.getElementById('inputBtn-3').remove()
-
-    var btn = document.getElementById('btn3')
-    btn.className = 'btn font-weight-bold'
-    btn.style.transition = '0.7s'
-}
-
-function choose_3(params) {
-    right_col_3.innerHTML = '<input type="text" id="inputChoosed" class = "conjunto"><button id="inputBtnChoosed" class = " btn ml-1 p-0 pl-1 pr-1 conjunto">Go</button>'
-    var btn = document.getElementById('btn3')
-    btn.style.background = "rgb(82, 126, 247)"
-    btn.style.borderRadius = '50px'
-    btn.style.color = 'white'
-    btn.style.fontSize = '25px'
-    btn.style.padding = '10px'
-}
-
-/*row 4*/
-let right_col_4 = document.getElementById('area-text-4')
-
-function show_4() {
-    right_col_4.innerHTML = '<input type="text" id="input-4" class = "conjunto"><button id="inputBtn-4" class = " btn ml-1 p-0 pl-1 pr-1 conjunto">Go</button>'
-}
-
-function notShow_4() {
-    document.getElementById('input-4').remove()
-    document.getElementById('inputBtn-4').remove()
-
-    var btn = document.getElementById('btn4')
-    btn.className = 'btn font-weight-bold'
-    btn.style.transition = '0.7s'
-}
-
-function choose_4(params) {
-    right_col_4.innerHTML = '<input type="text" id="inputChoosed" class = "conjunto"><button id="inputBtnChoosed" class = " btn ml-1 p-0 pl-1 pr-1 conjunto">Go</button>'
-    var btn = document.getElementById('btn4')
-    btn.style.background = "rgb(82, 126, 247)"
-    btn.style.borderRadius = '50px'
-    btn.style.color = 'white'
-    btn.style.fontSize = '25px'
-    btn.style.padding = '10px'
-}
 
 
